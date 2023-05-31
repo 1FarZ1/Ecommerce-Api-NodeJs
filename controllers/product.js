c9601d9b186c73b98daf36e2c518bbcbded95951
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-05-31 03:28:29 +0100

    product router

---
+++ controllers/product.js
@@ -0,0 +1,32 @@
+
+
+let createProduct = async (req,res)=>{
+}
+
+
+let getAllProducts = async (req,res)=>{
+}
+
+let uploadImage = async (req,res)=>{
+}
+
+
+let getSingleProduct = async (req,res)=>{
+}
+
+
+let updateProduct = async (req,res)=>{
+}
+
+let deleteProduct = async (req,res)=>{
+
+}
+
+module.exports = {
+    createProduct,
+    getAllProducts,
+    uploadImage,
+    getSingleProduct,
+    updateProduct,
+    deleteProduct
+}
--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,26 @@
-
-const productRouter = require("express").Router();
-
-productRouter.route("/").get();
+const router = require("express").Router();
 
 
-module.exports = productRouter;
+const { createProduct, uploadImage, updateProduct,deleteProduct,getSingleProduct,getAllProducts } = require("../controllers/Product");
+const { authorizeRoles, authMiddleware } = require("../middlewares/auth");
+
+// const { getSingleProductReviews } = require('../controllers/reviewController');
+
+router
+  .route('/')
+  .post([authMiddleware, authorizeRoles('admin')], createProduct)
+  .get(getAllProducts);
+
+router
+  .route('/uploadImage')
+  .post([authMiddleware, authMiddleware('admin')], uploadImage);
+
+router
+  .route('/:id')
+  .get(getSingleProduct)
+  .patch([authMiddleware, authMiddleware('admin')], updateProduct)
+  .delete([authMiddleware, authMiddleware('admin')], deleteProduct);
+
+// router.route('/:id/reviews').get(getSingleProductReviews);
+
+module.exports = router;
