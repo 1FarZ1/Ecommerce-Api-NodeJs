52c746771ea665d5dee5d93a92001e57c7a542bd
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-05-31 15:18:27 +0100

    review model and review controller done

---
+++ controllers/review.js
@@ -0,0 +1,24 @@
+
+
+let createReview = async (req,res)=>{
+}
+
+let updateReview = async (req,res)=>{
+}
+
+let deleteReview = async (req,res)=>{
+}
+
+let getAllReviews = async (req,res)=>{
+}
+
+let getSingleReview = async (req,res)=>{
+}
+
+module.exports = {
+    createReview,
+    updateReview,
+    deleteReview,
+    getAllReviews,
+    getSingleReview
+}
--- routes/review.js
+++ routes/review.js
@@ -1,7 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const { authMiddleware } = require('../middlewares/auth');
 
-const reviewRouter = require("express").Router();
+const {
+  createReview,
+  getAllReviews,
+  getSingleReview,
+  updateReview,
+  deleteReview,
+} = require('../controllers/review');
 
-reviewRouter.route("/").get();
+router.route('/').post(authMiddleware, createReview).get(getAllReviews);
 
+router
+  .route('/:id')
+  .get(getSingleReview)
+  .patch(authMiddleware, updateReview)
+  .delete(authMiddleware, deleteReview);
 
-module.exports = reviewRouter;
+module.exports = router;
