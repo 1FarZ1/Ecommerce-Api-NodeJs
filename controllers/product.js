fe6c2fd251c3b5fdc49630043dab94e5a6613cb8
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-05-31 03:52:21 +0100

    1/3 products controller

--- controllers/product.js
+++ controllers/product.js
@@ -1,10 +1,52 @@
-
+const Product = require('../models/product');
 
 let createProduct = async (req,res)=>{
+    const { name, price, description, category, company, colors, featured, freeShipping, inventory } = req.body;
+
+    let ProductToCreate = {
+        name,
+        price,
+        description,
+        category,
+        company,
+        colors,
+        featured,
+        freeShipping,
+        inventory,
+        user: req.user.userId,
+    }
+    try {
+        const product = await Product.create(ProductToCreate);
+        return res.status(201).json({
+            success: true,
+            product,
+        });
+    }
+    catch (error) {
+        return res.status(400).json({
+            success: false,
+            error: error.message,
+        });
+    }
 }
 
 
 let getAllProducts = async (req,res)=>{
+    try{
+        const products = await Product.find({});
+        return res.status(201).json({
+            success:true,
+            products,
+            count: products.length,
+        });
+
+    }
+    catch(error){
+        return res.status(400).json({
+            success: false,
+            error: error.message,
+        });
+    }
 }
 
 let uploadImage = async (req,res)=>{
--- routes/product.js
+++ routes/product.js
@@ -1,26 +1,24 @@
 const router = require("express").Router();
 
-
-const { createProduct, uploadImage, updateProduct,deleteProduct,getSingleProduct,getAllProducts } = require("../controllers/Product");
+const {createProduct,getAllProducts,uploadImage,getSingleProduct,updateProduct,deleteProduct} = require("../controllers/product");
 const { authorizeRoles, authMiddleware } = require("../middlewares/auth");
 
 // const { getSingleProductReviews } = require('../controllers/reviewController');
 
 router
   .route('/')
+  .get(getAllProducts)
   .post([authMiddleware, authorizeRoles('admin')], createProduct)
-  .get(getAllProducts);
-
-router
-  .route('/uploadImage')
-  .post([authMiddleware, authMiddleware('admin')], uploadImage);
-
-router
+  
+  router
   .route('/:id')
   .get(getSingleProduct)
   .patch([authMiddleware, authMiddleware('admin')], updateProduct)
   .delete([authMiddleware, authMiddleware('admin')], deleteProduct);
-
-// router.route('/:id/reviews').get(getSingleProductReviews);
+  
+  router
+    .route('/uploadImage')
+    .post([authMiddleware, authMiddleware('admin')],uploadImage);
+  // router.route('/:id/reviews').get(getSingleProductReviews);
 
 module.exports = router;
